faf1aa1c5253c4952023aed465c18042f74dbef1
JonBushell <JonathanBushell@hotmail.co.uk>
2025-05-07 02:17:35 +0100

    Updating bookbot python code

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from stats import number_of_words
 from stats import character_count
+from stats import sorted_dictionary
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
@@ -9,8 +10,19 @@ def get_book_text(path_to_file):
 def main ():
     num_words = number_of_words(get_book_text("books/frankenstein.txt"))
     num_characters = character_count(get_book_text("books/frankenstein.txt"))
-    print(f"{num_words} words found in the document")
-    print (num_characters)
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    sorted_chars = sorted_dictionary(num_characters)
+    for char_dict in sorted_chars:
+        if char_dict["char"].isalpha():
+            print(f"{char_dict['char']}: {char_dict['num']}")
+    print("============= END ===============")
+    
+    
+    
 
     
 main()
--- stats.py
+++ stats.py
@@ -11,3 +11,16 @@ def character_count(book_as_text):
             character_dict[char] = 1
     return character_dict
 
+def sorted_dictionary(character_dict):
+    sorted_list = []      #creating an empty list to make a list of dictionaries  
+    for char in character_dict:
+        count = character_dict[char]
+        char_dict = {"char": char, "num": count}
+        sorted_list.append(char_dict)
+    #List created, Will now handle sorting of the list in descending order
+    def sort_on(dict):
+        return dict["num"]
+
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+
